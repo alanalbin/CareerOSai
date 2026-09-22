5dfc5cfda4c6ea8d2026518324f2ef83bd3df24c
Alan Albin <alanalbin@example.com>
2026-09-22 21:28:19 +0530

    fix: restore missing string backticks

--- server/_core/vite.ts
+++ server/_core/vite.ts
@@ -36,8 +36,8 @@ export async function setupVite(app: Express, server: Server) {
       // always reload the index.html file from disk incase it changes
       let template = await fs.promises.readFile(clientTemplate, "utf-8");
       template = template.replace(
-        src="/src/main.tsx",
-        src="/src/main.tsx?v= + nanoid() + "
+        `src="/src/main.tsx"`,
+        `src="/src/main.tsx?v=${nanoid()}"`
       );
       const page = await vite.transformIndexHtml(url, template);
       res.status(200).set({ "Content-Type": "text/html" }).end(page);
@@ -55,7 +55,7 @@ export function serveStatic(app: Express) {
       : path.resolve(import.meta.dirname, "public");
   if (!fs.existsSync(distPath)) {
     console.error(
-      Could not find the build directory:  + distPath + , make sure to build the client first
+      `Could not find the build directory: ${distPath}, make sure to build the client first`
     );
   }
 
