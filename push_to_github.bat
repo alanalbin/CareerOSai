9f6e4249d5f4dcd9dfaa9144246cacbf67c8cab7
Alan Albin <alanalbin@example.com>
2026-09-23 12:04:18 +0530

    chore: update push_to_github.bat with CareerOSai repository URL

--- push_to_github.bat
+++ push_to_github.bat
@@ -12,7 +12,7 @@ git commit -m "Initial commit - Career OS Production Build"
 echo.
 echo Setting up GitHub connection...
 git branch -M main
-git remote add origin https://github.com/alanalbin/careeros.git
+git remote add origin https://github.com/alanalbin/CareerOSai.git
 
 echo.
 echo Pushing to GitHub (A login window may pop up!)...
